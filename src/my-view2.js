3287b7242311b8a92aae51c4284e885684e93e7d
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-07-19 15:43:42 +0800

    Update my-view2.js

--- src/my-view2.js
+++ src/my-view2.js
@@ -23,8 +23,31 @@ class MyView2 extends PolymerElement {
       </style>
 
       <div class="card">
-        <h1>Cookie Consent</h1>
-        <p>
+        <h1>Cookie policy</h1>
+<p>This cookie policy (&quot;Policy&quot;) describes what cookies are and how and they&#039;re being used by the <a target="_blank" rel="nofollow" href="https://melvinhgf.net">melvinhgf.net</a> website (&quot;Website&quot; or &quot;Service&quot;) and any of its related products and services (collectively, &quot;Services&quot;). This Policy is a legally binding agreement between you (&quot;User&quot;, &quot;you&quot; or &quot;your&quot;) and this Website operator (&quot;Operator&quot;, &quot;we&quot;, &quot;us&quot; or &quot;our&quot;). You should read this Policy so you can understand the types of cookies we use, the information we collect using cookies and how that information is used. It also describes the choices available to you regarding accepting or declining the use of cookies. For further information on how we use, store and keep your personal data secure, see our <a target="_blank" rel="nofollow" href="https://www.melvinhgf.net/privacy.html">privacy policy</a>.</p>
+<h2>What are cookies?</h2>
+<p>Cookies are small pieces of data stored in text files that are saved on your computer or other devices when websites are loaded in a browser. They are widely used to remember you and your preferences, either for a single visit (through a &quot;session cookie&quot;) or for multiple repeat visits (using a &quot;persistent cookie&quot;).</p>
+<p>Session cookies are temporary cookies that are used during the course of your visit to the Website, and they expire when you close the web browser.</p>
+<p>Persistent cookies are used to remember your preferences within our Website and remain on your desktop or mobile device even after you close your browser or restart your computer. They ensure a consistent and efficient experience for you while visiting the Website and Services.</p>
+<p>Cookies may be set by the Website (&quot;first-party cookies&quot;), or by third parties, such as those who serve content or provide advertising or analytics services on the Website (&quot;third party cookies&quot;). These third parties can recognize you when you visit our website and also when you visit certain other websites. <a target="_blank" href="https://www.websitepolicies.com/blog/cookies">Click here</a> to learn more about cookies and how they work.</p>
+<h2>What type of cookies do we use?</h2>
+<h3>Necessary cookies</h3>
+<p>Necessary cookies allow us to offer you the best possible experience when accessing and navigating through our Website and using its features. For example, these cookies let us recognize that you have created an account and have logged into that account to access the content.</p>
+<h3>Analytical cookies</h3>
+<p>These cookies enable us and third party services to collect aggregated data for statistical purposes on how our visitors use the Website. These cookies do not contain personal information such as names and email addresses and are used to help us improve your user experience of the Website.</p>
+<h3>Social media cookies</h3>
+<p>Third party cookies from social media sites (such as Facebook, Twitter, etc) let us track social network users when they visit or use the Website and Services, or share content, by using a tagging mechanism provided by those social networks.</p>
+<p>These cookies are also used for event tracking and remarketing purposes. Any data collected with these tags will be used in accordance with our and social networks’ privacy policies. We will not collect or share any personally identifiable information from the user.</p>
+<h2>What are your cookie options?</h2>
+<p>If you don’t like the idea of cookies or certain types of cookies, you can change your browser’s settings to delete cookies that have already been set and to not accept new cookies. Visit <a target="_blank" href="https://www.internetcookies.com" rel="noopener">internetcookies.com</a> to learn more about how to do this.</p>
+<h2>Changes and amendments</h2>
+<p>We reserve the right to modify this Policy or its terms relating to the Website and Services at any time, effective upon posting of an updated version of this Policy on the Website. When we do, we will post a notification on the main page of the Website. Continued use of the Website and Services after any such changes shall constitute your consent to such changes.</p>
+<h2>Acceptance of this policy</h2>
+<p>You acknowledge that you have read this Policy and agree to all its terms and conditions. By accessing and using the Website and Services you agree to be bound by this Policy. If you do not agree to abide by the terms of this Policy, you are not authorized to access or use the Website and Services. This cookie policy was created with the <a target="_blank" href="https://www.websitepolicies.com/cookie-policy-generator">cookie policy generator</a>.</p>
+<h2>Contacting us</h2>
+<p>If you have any questions about this Policy, please contact us.</p>
+<p>This document was last updated on April 3, 2019</p>
+       
          <!-- Cookie Consent by https://www.TermsFeed.com -->
 <script type="text/javascript" src="//www.termsfeed.com/public/cookie-consent/4.0.0/cookie-consent.js" charset="UTF-8"></script>
 <script type="text/javascript" charset="UTF-8">
@@ -35,7 +58,7 @@ cookieconsent.run({"notice_banner_type":"headline","consent_type":"express","pal
 
 <noscript>Cookie Consent by <a href="https://www.TermsFeed.com/" rel="nofollow noopener">TermsFeed</a></noscript>
 <!-- End Cookie Consent -->
-        </p>
+        
       </div>
     `;
   }
