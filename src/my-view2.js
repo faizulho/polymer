029f4d6a7afa369da2c020ccb80912bbab273f47
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-07-19 15:00:48 +0800

    Update my-view2.js

--- src/my-view2.js
+++ src/my-view2.js
@@ -24,12 +24,19 @@ class MyView2 extends PolymerElement {
 
       <div class="card">
         <div class="circle">2</div>
-        <h1>View Two</h1>
+        <h1>Cookie Consent</h1>
         <p>Ea duis bonorum nec, falli paulo aliquid ei eum.</p>
         <p>
-          Id nam odio natum malorum, tibique copiosae expetenda mel ea.Detracto
-          suavitate repudiandae no eum. Id adhuc minim soluta nam.Id nam odio
-          natum malorum, tibique copiosae expetenda mel ea.
+         <!-- Cookie Consent by https://www.TermsFeed.com -->
+<script type="text/javascript" src="//www.termsfeed.com/public/cookie-consent/4.0.0/cookie-consent.js" charset="UTF-8"></script>
+<script type="text/javascript" charset="UTF-8">
+document.addEventListener('DOMContentLoaded', function () {
+cookieconsent.run({"notice_banner_type":"headline","consent_type":"express","palette":"light","language":"en","page_load_consent_levels":["strictly-necessary"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"website_name":"MELVINHGF.NET: Your Online Solutions Provider","website_privacy_policy_url":"https://www.termsfeed.com/live/dfff3e93-4e85-4b8d-9aa4-e6658c9b1c5d"});
+});
+</script>
+
+<noscript>Cookie Consent by <a href="https://www.TermsFeed.com/" rel="nofollow noopener">TermsFeed</a></noscript>
+<!-- End Cookie Consent -->
         </p>
       </div>
     `;
