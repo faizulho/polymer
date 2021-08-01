0444437644fd83dd4199640cbf7f53e8918a4ff7
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-08-02 04:09:35 +0800

    Update my-view404.js

--- src/my-view404.js
+++ src/my-view404.js
@@ -8,19 +8,21 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
+
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import './shared-styles.js';
 
 class MyView404 extends PolymerElement {
   static get template() {
     return html`
-      <style>
+      <style include="shared-styles">
         :host {
           display: block;
 
-          padding: 10px 20px;
+          padding: 10px;
         }
       </style>
+
       <div class="card">
       <p>Oops you hit a 404. <a href="[[rootPath]]">Head back to home.</a></p>
       </div>
