cd95b0df641a43e834c7536890a646090a6d7c2c
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-08-02 02:07:00 +0800

    Update my-view9.js

--- src/my-view9.js
+++ src/my-view9.js
@@ -21,73 +21,50 @@ class MyView9 extends PolymerElement {
         }
       </style>
       <div class="card">
-<h1>Return and Refund Policy</h1>
+<h1>Disclaimer</h1>
 <p>Last updated: August 01, 2021</p>
-<p>Thank you for shopping at MELVINHGF.NET: Your Online Solutions Provider websites and apps.</p>
-<p>If, for any reason, You are not completely satisfied with a purchase We invite You to review our policy on refunds and returns.</p>
-<p>The following terms are applicable for any products that You purchased with Us at our official and subsidiary websites and apps.</p>
-<h1>Interpretation and Definitions</h1>
-<h2>Interpretation</h2>
-<p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
-<h2>Definitions</h2>
-<p>For the purposes of this Return and Refund Policy:</p>
+<h2>Interpretation and Definitions</h2>
+<h3>Interpretation</h3>
+<p>The words of which the initial letter is capitalized have meanings defined under the following conditions.
+The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
+<h3>Definitions</h3>
+<p>For the purposes of this Disclaimer:</p>
 <ul>
-<li>
-<p><strong>Company</strong> (referred to as either &quot;the Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Agreement) refers to MELVINHGF.NET: Your Online Solutions Provider, 22 Sin Ming Lane #06-76 Singapore 573969.</p>
-</li>
-<li>
-<p><strong>Goods</strong> refer to the items and services offered for sale on the Service. They can be physical goods or digital goods.</p>
-</li>
-<li>
-<p><strong>Orders</strong> mean a request by You to purchase Goods from Us.</p>
-</li>
-<li>
-<p><strong>Service</strong> refers to the Website.</p>
-</li>
-<li>
-<p><strong>Website</strong> refers to MELVINHGF.NET: Your Online Solutions Provider Website, accessible from <a href="https://melvinhgf.business.site" rel="external nofollow noopener" target="_blank">https://melvinhgf.business.site</a> or its other official and subsidiary websites and apps.</p>
-</li>
-<li>
-<p><strong>You</strong> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</p>
-</li>
+<li><strong>Company</strong> (referred to as either &quot;the Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Disclaimer) refers to MELVINHGF.NET: Your Online Solutions Provider, 22 Sin Ming Lane #06-76 Singapore 573969.</li>
+<li><strong>Service</strong> refers to the Website or the Application or both of MELVINHGF.NET: Your Online Solutions Provider or its subsidiaries.</li>
+<li><strong>You</strong> means the individual accessing the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</li>
+<li><strong>Website</strong> refers to MELVINHGF.NET: Your Online Solutions Provider, accessible from <a href="https://melvinhgf.business.site" rel="external nofollow noopener" target="_blank">https://melvinhgf.business.site</a> or its other official websites, including its subsidiaries'.</li>
+<li><strong>Application</strong> means the software program and apps provided by the Company downloaded by You on any electronic device whose copyright is under MELVINHGF.NET: Your Online Solutions Provider, its subsidiaries, appointed agents and persons.</li>
 </ul>
-<h1>Your Order Cancellation Rights</h1>
-<p>You are entitled to cancel Your Order within 14 days without giving any reason for doing so.</p>
-<p>The deadline for cancelling an Order is 14 days from the date on which You received the Goods or on which a third party you have appointed, who is not the carrier, takes possession of the product delivered.</p>
-<p>In order to exercise Your right of cancellation, You must inform Us of your decision by means of a clear statement. You can inform us of your decision by:</p>
-<ul>
-<li>Email: [support@melvinhgf.freshdesk.com](mailto:support@melvinhgf.freshdesk.com)</li>
-<li>[Contact Us](https://docs.melvinhgf.net/view6) page</li>
-</ul>
-<p>We will reimburse You no later than 14 days from the day on which We receive the returned Goods. We will use the same means of payment as You used for the Order, and You will not incur any fees for such reimbursement.</p>
-<h1>Conditions for Returns</h1>
-<p>In order for the Goods to be eligible for a return, please make sure that:</p>
-<ul>
-<li>The physical Goods were purchased in the last 14 days</li>
-<li>The physical Goods are in the original packaging</li>
-</ul>
-<p>The following Goods **cannot** be returned:</p>
-<ul>
-<li>The supply of Goods made to Your specifications or clearly personalized.</li>
-<li>The supply of Goods which according to their nature are not suitable to be returned, deteriorate rapidly or where the date of expiry is over.</li>
-<li>The supply of Goods which are not suitable for return due to health protection or hygiene reasons and were unsealed after delivery.</li>
-<li>The supply of Goods which are, after delivery, according to their nature, inseparably mixed with other items.</li>
-<li>The supply of Goods which are, **digital**, either already downloaded or sold to You or Your appointed agents and users.</li>
-</ul>
-<p>We reserve the right to refuse returns of any merchandise that does not meet the above return conditions in our sole discretion.</p>
-<p>Only regular priced Goods may be refunded. Unfortunately, Goods on sale cannot be refunded. This exclusion may not apply to You if it is not permitted by applicable law.</p>
-<h1>Returning Goods</h1>
-<p>You are responsible for the cost and risk of returning the Goods to Us. You should contact us for further advice at [support@melvinhgf.freshdesk com](mailto:support@melvinhgf.freshdesk com) or [Contact Us](https://docs.melvinhgf.net/view6) page.</p>
-<p>Please note that digital products are not eligible for refunds once it has been downloaded.</p>
-<p>We cannot be held responsible for Goods damaged or lost in return shipment. Therefore, We recommend an insured and trackable mail service. We are unable to issue a refund without actual receipt of the Goods or proof of received return delivery.</p>
-<h1>Gifts</h1>
-<p>If the Goods were marked as a gift when purchased and then shipped directly to you, You'll receive a gift credit for the value of your return. Once the returned product is received, a gift certificate will be mailed to You.</p>
-<p>If the Goods weren't marked as a gift when purchased, or the gift giver had the Order shipped to themselves to give it to You later, We will send the refund to the gift giver.</p>
+<h2>Disclaimer</h2>
+<p>The information contained on the Service is for general information purposes only.</p>
+<p>The Company assumes no responsibility for errors or omissions in the contents of the Service.</p>
+<p>In no event shall the Company be liable for any special, direct, indirect, consequential, or incidental damages or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of or in connection with the use of the Service or the contents of the Service. The Company reserves the right to make additions, deletions, or modifications to the contents on the Service at any time without prior notice. </p>
+<p>The Company does not warrant that the Service is free of viruses or other harmful components.</p>
+<h2>External Links Disclaimer</h2>
+<p>The Service may contain links to external websites that are not provided or maintained by the Company. Some of these links to external websites may be links of affiliates related to the Company under official established private or public relationships.</p>
+<p>Please note that the Company does not guarantee the accuracy, relevance, timeliness, or completeness of any information on these external websites.</p>
+<h2>Errors and Omissions Disclaimer</h2>
+<p>The information given by the Service is for general guidance on matters of interest only. Even if the Company takes every precaution to ensure that the content of the Service is both current and accurate, errors can occur. Plus, given the changing nature of laws, rules and regulations, there may be delays, omissions or inaccuracies in the information contained on the Service.</p>
+<p>The Company is not responsible for any errors or omissions, or for the results obtained from the use of this information.</p>
+<h2>Fair Use Disclaimer</h2>
+<p>The Company may use copyrighted material which has not always been specifically authorized by the copyright owner. The Company is making such material available for criticism, comment, news reporting, teaching, scholarship, or research.</p>
+<p>The Company believes this constitutes a &quot;fair use&quot; of any such copyrighted material as provided for in section 107 of the United States Copyright law.</p>
+<p>If You wish to use copyrighted material from the Service for your own purposes that go beyond fair use, You must obtain permission from the copyright owner.</p>
+<h2>Views Expressed Disclaimer</h2>
+<p>The Service may contain views and opinions which are those of the authors and do not necessarily reflect the official policy or position of any other author, agency, organization, employer or company, including the Company.</p>
+<p>Comments published by users are their sole responsibility and the users will take full responsibility, liability and blame for any libel or litigation that results from something written in or as a direct result of something written in a comment. The Company is not liable for any comment published by users and reserve the right to delete any comment for any reason whatsoever.</p>
+<h2>No Responsibility Disclaimer</h2>
+<p>The information on the Service is provided with the understanding that the Company is not herein engaged in rendering legal, accounting, tax, or other professional advice and services. As such, it should not be used as a substitute for consultation with professional accounting, tax, legal or other competent advisers.</p>
+<p>In no event shall the Company or its suppliers be liable for any special, incidental, indirect, or consequential damages whatsoever arising out of or in connection with your access or use or inability to access or use the Service.</p>
+<h2>&quot;Use at Your Own Risk&quot; Disclaimer</h2>
+<p>All information in the Service is provided &quot;as is&quot;, with no guarantee of completeness, accuracy, timeliness or of the results obtained from the use of this information, and without warranty of any kind, express or implied, including, but not limited to warranties of performance, merchantability and fitness for a particular purpose.</p>
+<p>The Company will not be liable to You or anyone else for any decision made or action taken in reliance on the information given by the Service or for any consequential, special or similar damages, even if advised of the possibility of such damages.</p>
 <h2>Contact Us</h2>
-<p>If you have any questions about our Returns and Refunds Policy, please contact us:</p>
+<p>If you have any questions about this Disclaimer, You can contact Us:</p>
 <ul>
-<li>By email: [support@melvinhgf.freshdesk.com](mailto:support@melvinhgf.freshdesk.com)</li>
-<li>[Contact Us](https://docs.melvinhgf.net/view6) page</li>
+<li>Email: <a href="mailto:support@melvinhgf.freshdesk.com" rel="external nofollow noopener" target="_blank">support@melvinhgf.freshdesk.com</a></li>
+<li><a href="view6">Contact Us</a> page</li>
 </ul>
 </div>
     `;
